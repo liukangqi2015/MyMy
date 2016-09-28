d0b4da1b8557b95717ebd4a8380f8edb36ea2127
liukangqi2015 <330620143@qq.com>
2016-09-28 15:07:59 +0800

    提交一些基本框架

--- app/src/main/java/com/liu/mymy/MainActivity.java
+++ app/src/main/java/com/liu/mymy/MainActivity.java
@@ -1,13 +1,91 @@
 package com.liu.mymy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.flyco.tablayout.SegmentTabLayout;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+import com.liu.mymy.fragment.SimpleCardFragment;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG=MainActivity.class.getSimpleName();
+    @BindView(R.id.stl)
+    SegmentTabLayout mSTabLayout;
+    @BindView(R.id.vp)
+    ViewPager mViewPager;
+    private String[] mTitles = {"新闻", "干货", "福利"};
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private MyPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        for (String title : mTitles) {
+            mFragments.add(SimpleCardFragment.getInstance(title));
+        }
+
+        mAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mAdapter);
+        mSTabLayout.setTabData(mTitles);
+        mSTabLayout.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+                mViewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onTabReselect(int position) {
+
+            }
+        });
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//                Log.e(TAG,"position"+position+"positionOffset"+positionOffset+"positionOffsetPixels"+positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                mSTabLayout.setCurrentTab(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        mViewPager.setCurrentItem(1);
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
     }
 }
